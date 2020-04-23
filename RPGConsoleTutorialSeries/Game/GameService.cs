c17aba2172e78712eb26fcea307212c8d963acc3
Todd Vance <tvance@vant4ge.com>
2020-04-22 19:47:29 -0500

    first commit

---
+++ RPGConsoleTutorialSeries/Game/GameService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+using RPGConsoleTutorialSeries.Adventures;
+
+namespace RPGConsoleTutorialSeries.Game
+{
+    public class GameService
+    {
+        public void StartGame()
+        {
+            var basePath = $"{AppDomain.CurrentDomain.BaseDirectory}adventures";
+            var initialAdventure = new Adventure();
+
+            if (File.Exists($"{basePath}\\initial.json"))
+            {
+                var directory = new DirectoryInfo(basePath);
+                var initialJsonFile = directory.GetFiles("initial.json");
+
+                using (StreamReader fi = File.OpenText(initialJsonFile[0].FullName))
+                {
+                    initialAdventure = JsonConvert.DeserializeObject<Adventure>(fi.ReadToEnd());
+                }
+
+                Console.WriteLine($"Adventure : {initialAdventure.Title}");
+                Console.WriteLine($"Description : {initialAdventure.Description}");
+            }
+        }
+    }
+}
